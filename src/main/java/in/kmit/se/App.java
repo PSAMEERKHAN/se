40ddb1479a31a729ba598a35ec61a464b8b25358
Sameer <20bd1a0575@gmail.com>
2022-11-16 12:17:34 +0530

    project

--- src/main/java/in/kmit/se/App.java
+++ src/main/java/in/kmit/se/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "this is se lab internal 1 exam the created project is exam maven" );
     }
 }
